8ae1755260d640e1ace805ee956beb2b681d2c83
Yuji Shimojima <yuji.shimojima.s@gmail.com>
2024-04-08 17:35:36 +0200

    linux version

--- init.el
+++ init.el
@@ -210,8 +210,13 @@
 ;; (global-set-key [f7] 'clipboard-yank)
 
 ;;for linux
-;;(setq select-enable-primary nil)
-;;(setq select-enable-clipboard t)
+;(setq select-enable-primary nil)
+(setq x-select-enable-clipboard t)
+(leaf xclip
+  :ensure t
+  :config
+  ;; xclip-mode を有効にする
+  (xclip-mode 1))
 
 ;; for windows
 ;; (cond (window-system
@@ -219,40 +224,43 @@
 ;;        ))
 
 
-;; for linux(wsl) new
-;; sync with x clipboard
-(unless window-system
-  (when (getenv "DISPLAY")
-    ;; Callback for when user cuts
-    (defun xsel-cut-function (text &optional push)
-      ;; Insert text to temp-buffer, and "send" content to xsel stdin
-      (with-temp-buffer
-        (insert text)
-        ;; I prefer using the "clipboard" selection (the one the
-        ;; typically is used by c-c/c-v) before the primary selection
-        ;; (that uses mouse-select/middle-button-click)
-        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
-    ;; Call back for when user pastes
-    (defun xsel-paste-function()
-      ;; Find out what is current selection by xsel. If it is different
-      ;; from the top of the kill-ring (car kill-ring), then return
-      ;; it. Else, nil is returned, so whatever is in the top of the
-      ;; kill-ring will be used.
-      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
-        (unless (string= (car kill-ring) xsel-output)
-          xsel-output )))
-    ;; Attach callbacks to hooks
-    (setq interprogram-cut-function 'xsel-cut-function)
-    (setq interprogram-paste-function 'xsel-paste-function)
-    ;; Idea from
-    ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
-    ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
-    ))
+;; ;; for linux(wsl) new
+;; ;; sync with x clipboard
+;; (unless window-system
+;;   (when (getenv "DISPLAY")
+;;     ;; Callback for when user cuts
+;;     (defun xsel-cut-function (text &optional push)
+;;       ;; Insert text to temp-buffer, and "send" content to xsel stdin
+;;       (with-temp-buffer
+;;         (insert text)
+;;         ;; I prefer using the "clipboard" selection (the one the
+;;         ;; typically is used by c-c/c-v) before the primary selection
+;;         ;; (that uses mouse-select/middle-button-click)
+;;         (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+;;     ;; Call back for when user pastes
+;;     (defun xsel-paste-function()
+;;       ;; Find out what is current selection by xsel. If it is different
+;;       ;; from the top of the kill-ring (car kill-ring), then return
+;;       ;; it. Else, nil is returned, so whatever is in the top of the
+;;       ;; kill-ring will be used.
+;;       (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+;;         (unless (string= (car kill-ring) xsel-output)
+;;           xsel-output )))
+;;     ;; Attach callbacks to hooks
+;;     (setq interprogram-cut-function 'xsel-cut-function)
+;;     (setq interprogram-paste-function 'xsel-paste-function)
+;;     ;; Idea from
+;;     ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
+;;     ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
+;;     ))
 
 
-
+;;editer colour 
 (setq auto-mode-alist (cons '("\\.cu$" . c++-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.cuh$" . c++-mode) auto-mode-alist))
+(leaf nix-mode
+  :ensure t
+  :mode "\\.nix\\'")
 
 
 (require 'flymake)
