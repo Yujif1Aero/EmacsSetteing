be45d6e5d378f4fbf52536e175e0d2c8dca1ef01
Yuji_morgen_ws1 <shimojima@morgenrot.net>
2024-04-23 06:22:50 +0900

    changed to ehell from bash

--- init.el
+++ init.el
@@ -229,33 +229,33 @@
 
 ;; ;; for linux(wsl) new
 ;; ;; sync with x clipboard
-;; (unless window-system
-;;   (when (getenv "DISPLAY")
-;;     ;; Callback for when user cuts
-;;     (defun xsel-cut-function (text &optional push)
-;;       ;; Insert text to temp-buffer, and "send" content to xsel stdin
-;;       (with-temp-buffer
-;;         (insert text)
-;;         ;; I prefer using the "clipboard" selection (the one the
-;;         ;; typically is used by c-c/c-v) before the primary selection
-;;         ;; (that uses mouse-select/middle-button-click)
-;;         (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
-;;     ;; Call back for when user pastes
-;;     (defun xsel-paste-function()
-;;       ;; Find out what is current selection by xsel. If it is different
-;;       ;; from the top of the kill-ring (car kill-ring), then return
-;;       ;; it. Else, nil is returned, so whatever is in the top of the
-;;       ;; kill-ring will be used.
-;;       (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
-;;         (unless (string= (car kill-ring) xsel-output)
-;;           xsel-output )))
-;;     ;; Attach callbacks to hooks
-;;     (setq interprogram-cut-function 'xsel-cut-function)
-;;     (setq interprogram-paste-function 'xsel-paste-function)
-;;     ;; Idea from
-;;     ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
-;;     ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
-;;     ))
+(unless window-system
+  (when (getenv "DISPLAY")
+    ;; Callback for when user cuts
+    (defun xsel-cut-function (text &optional push)
+      ;; Insert text to temp-buffer, and "send" content to xsel stdin
+      (with-temp-buffer
+        (insert text)
+        ;; I prefer using the "clipboard" selection (the one the
+        ;; typically is used by c-c/c-v) before the primary selection
+        ;; (that uses mouse-select/middle-button-click)
+        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    ;; Call back for when user pastes
+    (defun xsel-paste-function()
+      ;; Find out what is current selection by xsel. If it is different
+      ;; from the top of the kill-ring (car kill-ring), then return
+      ;; it. Else, nil is returned, so whatever is in the top of the
+      ;; kill-ring will be used.
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+        (unless (string= (car kill-ring) xsel-output)
+          xsel-output )))
+    ;; Attach callbacks to hooks
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)
+    ;; Idea from
+    ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
+    ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
+    ))
 
 
 ;;editer colour
@@ -280,10 +280,10 @@
 ;;   (package-initialize))
 
 
-;; ;; xterm のマウスイベントを取得する
-;; (xterm-mouse-mode t)
-;; ;; マウスホイールを取得する
-;; (mouse-wheel-mode t)
+;; xterm のマウスイベントを取得する
+(xterm-mouse-mode t)
+;; マウスホイールを取得する
+(mouse-wheel-mode t)
 
 
 
@@ -530,9 +530,9 @@
   :custom
   ;; shell-popで使用するシェルのタイプを設定します。ここではeshellを使用します。
 
-  ;; (shell-pop-shell-type . '("eshell" "*eshell*" (lambda () (eshell))))
+   (shell-pop-shell-type . '("eshell" "*eshell*" (lambda () (eshell))))
   ;;   (shell-pop-shell-type . '("term" "*term*" (lambda () (term "/run/current-system/sw/bin/zsh"))))
-     (shell-pop-shell-type . '("term" "*term*" (lambda () (term "/bin/bash"))))
+  ;;   (shell-pop-shell-type . '("term" "*term*" (lambda () (term "/bin/bash"))))
 
 
   ;; 例: (shell-pop-window-size . 30) ; ウィンドウのサイズを30%に設定
