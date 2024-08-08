dfcd686e0553e75f024012d0b8c060042de052e6
Yuji Sam Shimojima <yuji.shimojima@kit.edu>
2024-08-08 23:50:29 +0200

    bug fix project-root

--- init.el
+++ init.el
@@ -1128,11 +1128,11 @@
     (projectile-mode +1)
     
     ;; プロジェクトのルートディレクトリに `default-directory` を設定する関数を追加
-    ;; (defun set-default-directory-to-project-root ()
-    ;;   "Set `default-directory` to the root of the project."
-    ;;   (let ((project-root (projectile-project-root)))
-    ;;     (when project-root
-    ;;       (setq default-directory project-root))))
+    (defun set-default-directory-to-project-root ()
+      "Set `default-directory` to the root of the project."
+      (let ((project-root (projectile-project-root)))
+        (when project-root
+          (setq default-directory project-root))))
 
     ;; ;; find-file-hook に関数を追加
      (add-hook 'find-file-hook 'set-default-directory-to-project-root)
