a7bc8b0655ad778d01287acf945c6bf0a538d7da
Yuji <shimojima@morgenrot.net>
2025-08-01 16:16:24 +0900

    bug fixed copy and paste

--- init_linux.el
+++ init_linux.el
@@ -1,11 +1,25 @@
+;; 端末(非GUI)でのマウス
+(unless (display-graphic-p)
+  (xterm-mouse-mode 1))
+
+;; --- クリップボード設定 ---
+(leaf clipetty
+  :straight t
+  :when (not (display-graphic-p))        ; TTY だけ
+  :config (global-clipetty-mode 1))      ; Emacs → ローカル に OSC52 でコピー
+
 (leaf xclip
   :straight t
-  :config
-  ;; xclip-mode を有効にする
-  (xclip-mode 1))
+  :when (display-graphic-p)              ; GUI のときだけ
+  :config (xclip-mode 1))
 
-(setq select-enable-clipboard t)
-(setq select-enable-primary t)
+;; TTY では念のため xclip を必ず OFF（DISPLAY が見えていても切る）
+(unless (display-graphic-p)
+  (with-eval-after-load 'xclip (xclip-mode -1)))
+
+
+;;(setq select-enable-primary t)
+
 
 (leaf eshell-git-prompt
   :straight t
@@ -15,4 +29,5 @@
 
 ;; C-z で Emacs をサスペンドする
 (global-set-key (kbd "C-z") 'suspend-emacs)
+
 
